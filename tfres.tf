1c9e68193edc4626c95fbd689927260ae1987cec
jingizoo <jalaj.sm@gmail.com>
2025-06-02 06:58:32 +0530

    Create tfres.tf

---
+++ tfres.tf
@@ -0,0 +1,15 @@
+resource "google_bigquery_dataset" "peoplesoft_archive" {
+  dataset_id                 = "peoplesoft_archive"
+  project                    = var.project_id          # or hard-code
+  location                   = "US"                    # e.g. US, EU, asia-south1…
+  description                = "PeopleSoft archive tables"
+  default_table_expiration_ms = 90 * 24 * 60 * 60 * 1000  # 90 days (optional)
+
+  # Safety net: don’t let ‘terraform destroy’ drop the data by mistake
+  delete_contents_on_destroy = false
+
+  labels = {
+    env  = var.env         # dev, qa, prod …
+    team = "accounting"
+  }
+}
